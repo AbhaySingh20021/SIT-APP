2dbb869391e8e6774c89e76bcd1013b446583aa9
Abhay <Abhaysengar3250@gmail.com>
2021-04-20 11:30:17 +0530

    sidebar implemented

--- app/src/main/java/com/example/sitpoll/Loginactivity.java
+++ app/src/main/java/com/example/sitpoll/Loginactivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
@@ -13,24 +14,26 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class Loginactivity extends AppCompatActivity {
-    EditText username;
+    EditText username2;
     EditText password;
 
 
     public void login(View view){
 
 
-        if(username.getText().toString().matches("") || password.getText().toString().matches("") ){
+        if(username2.getText().toString().matches("") || password.getText().toString().matches("") ){
             Toast.makeText(this, "Username or Password required", Toast.LENGTH_SHORT).show();
-
 
         }
         else {
-            ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
+            ParseUser.logInInBackground(username2.getText().toString(), password.getText().toString(), new LogInCallback() {
                 @Override
                 public void done(ParseUser user, ParseException e) {
                     if (user != null) {
                         Toast.makeText(Loginactivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getApplicationContext(),mainvoteactivity.class);
+                        startActivity(intent);
 
                     } else Toast.makeText(Loginactivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
 
@@ -46,7 +49,11 @@ public class Loginactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginactivity);
-        username= findViewById(R.id.personName2);
+        username2= findViewById(R.id.personName2);
         password= findViewById(R.id.password2);
+
+
+
+
     }
 }
