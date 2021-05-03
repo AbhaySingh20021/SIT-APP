f77d5f1f7bfb8bb372866a18734665e51b20d1e0
Abhay <Abhaysengar3250@gmail.com>
2021-05-03 10:18:05 +0530

    Answer selection implemented

--- app/src/main/java/com/example/sitpoll/ui/showPolls/ShowOnePoll.java
+++ app/src/main/java/com/example/sitpoll/ui/showPolls/ShowOnePoll.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.sitpoll.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,18 +23,45 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class ShowOnePoll extends AppCompatActivity {
+    ListView listView;
+    FirebaseAuth auth;
+    ArrayAdapter arrayAdapter;
+    Intent intent;
+
+
+    ArrayList<String> options = new ArrayList<>();
+
+
+
+    public void selectans(int position){
+
+        listView.setItemChecked(position,true);
+
+        String quesUuid= intent.getStringExtra("quesUuid");
+        String user_uid= auth.getCurrentUser().getUid();
+        FirebaseDatabase.getInstance().getReference().child("UserData").child(user_uid).child("pollsanswered").child(quesUuid).setValue(position);
+
+            }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_one_poll);
         TextView questiontopic= findViewById(R.id.textView5);
+
         TextView from= findViewById(R.id.textView7);
-        ListView listView = findViewById(R.id.listview1);
+        listView = findViewById(R.id.listview1);
+        intent = getIntent();
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+
         ArrayList<String> options = new ArrayList<>();
+        auth= FirebaseAuth.getInstance();
+        String user_uid= auth.getCurrentUser().getUid();
+
         Intent intent = getIntent();
         String quesUuid= intent.getStringExtra("quesUuid");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice,options);
+         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice,options);
         FirebaseDatabase.getInstance().getReference()
                 .child("Polls").child(quesUuid).addValueEventListener(new ValueEventListener() {
             @Override
@@ -49,8 +80,34 @@ public class ShowOnePoll extends AppCompatActivity {
 
             }
         });
+
         arrayAdapter.notifyDataSetChanged();
         listView.setAdapter(arrayAdapter);
+
+        FirebaseDatabase.getInstance().getReference().child("UserData").child(user_uid).child("pollsanswered").child(quesUuid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                long option= (long) snapshot.getValue();
+                listView.setItemChecked((int) option,true);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectans(position);
+            }
+        });
+
 
 
     }
